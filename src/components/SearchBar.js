2f358414e996e7650ffc3d7ce9679120e0521487
lzambranoa <lzambranoa.software@gmail.com>
2020-12-07 16:18:10 -0500

    Creacion del Searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const SearchBar = () => {
+
+    return (
+        <div className="container text-center">
+            <div className="row" >
+                <div className="col-md-4" >
+                    <h1>LabCommerce App</h1>
+                </div>
+                <div className="col-md-8" >
+                    <input 
+                    type="text" 
+                    placeholder="Busca tus productos..."
+                    />
+                    <button>Buscar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
