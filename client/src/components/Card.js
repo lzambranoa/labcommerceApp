addc19e1f5fa08471b679ac8f4c280c58bc9e567
lzambranoa <lzambranoa.software@gmail.com>
2021-01-20 20:10:14 -0500

    Enlace del boton de información

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-export const Card = ({product}) => {
+export const Card = ({ product }) => {
 
     const url = product.permalink;
 
     return (
-        <div className="card m-2" style={{maxWidth: 320}}>
+        <div className="card m-2" style={{ maxWidth: 320 }}>
             <div className="row g-0 no-gutters">
                 <div className="col-md-4">
-                    <img src={ product.thumbnail } alt={ product.title } className="card-img pt-2"/>
+                    <img src={product.thumbnail} alt={product.title} className="card-img pt-2" />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">{ product.title  }</h5>
-                        <p className="card-text">Precio: ${ product.price }</p>
-                        <p className="card-text">Estado: { product.condition }</p>
-                        <p className="card-text">Stock: { product.available_quantity }</p>
-                            <button className="btn btn-primary" >Comprar</button>
+                        <h5 className="card-title">{product.title}</h5>
+                        <p className="card-text">Precio: ${product.price}</p>
+                        <p className="card-text">Estado: {product.condition}</p>
+                        <p className="card-text">Stock: {product.available_quantity}</p>
+                        <a href={url}>Más Información...</a>
                     </div>
                 </div>
             </div>
